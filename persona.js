89f55064e384b3e9c94bf35556f425b96e0c62dd
ninminnin <elinabarrenechea2810@gmail.com>
2025-11-30 04:36:55 -0500

    Update persona.js

--- persona.js
+++ persona.js
@@ -1,1 +1,14 @@
+const juridica = document.getElementById("form-juridica");
+const natural = document.getElementById("form-natural");
 
+document.querySelectorAll("input[name='tipo']").forEach(r => {
+  r.addEventListener("change", e => {
+    if (e.target.value === "juridica") {
+      juridica.classList.remove("oculto");
+      natural.classList.add("oculto");
+    } else {
+      natural.classList.remove("oculto");
+      juridica.classList.add("oculto");
+    }
+  });
+});
