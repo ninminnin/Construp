5963f409307260d99a19093a1c05d120f238b203
ninminnin <elinabarrenechea2810@gmail.com>
2025-11-30 00:22:55 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,23 @@
+function validarLogin(event) {
+  event.preventDefault();
 
+  const USUARIO_CORRECTO = "A&C Constructora";
+  const usuarioIngresado = document.getElementById("usuario").value;
+
+  const CLAVE_CORRECTA = "construp";
+  const passwordInput = document.getElementById("password").value;
+
+  if (
+    usuarioIngresado === USUARIO_CORRECTO &&
+    passwordInput === CLAVE_CORRECTA
+  ) {
+    alert("Bienvenido a Construp");
+    window.location.href = "inicio.html";
+  } else {
+    alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
+    document.getElementById("usuario").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("usuario").focus();
+  }
+  return false;
+}
