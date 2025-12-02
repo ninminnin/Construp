9afa6a16b14850d489a51a940b8d277f8bcee68c
ninminnin <elinabarrenechea2810@gmail.com>
2025-12-02 02:09:54 -0500

    Update menu-publi.js

--- menu-publi.js
+++ menu-publi.js
@@ -2,14 +2,13 @@ document.addEventListener("click", function (e) {
   const button = e.target.closest(".menu-btn");
   const menus = document.querySelectorAll(".menu-opciones");
 
-  // Cerrar todos los menús antes de abrir otro
+  // Cierra anteriores
   menus.forEach(m => (m.style.display = "none"));
 
-  // Si se hace clic en el botón de 3 puntos
   if (button) {
     const menu = button.parentElement.querySelector(".menu-opciones");
 
-    // Mostrar el menú correspondiente
+    // MUESTRA MENU
     if (menu) {
       menu.style.display = "block";
     }
